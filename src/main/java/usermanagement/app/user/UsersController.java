048eadfc5a44d3cc737d5b3811840345cfabbec4
GhenadieCebanu <cebanu.ghenadie@gmail.com>
2020-03-17 15:26:24 +0200

    add log

--- src/main/java/usermanagement/app/user/UsersController.java
+++ src/main/java/usermanagement/app/user/UsersController.java
@@ -2,6 +2,7 @@ package usermanagement.app.user;
 
 import java.util.ArrayList;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/get-out/users")
+@Slf4j
 public class UsersController {
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public List<User> getAllUsers() {
+    log.info("UM: Get all users");
     List<User> users = new ArrayList<>();
     users.add(User.builder().id(1L).email("mike@pearson.com").build());
     users.add(User.builder().id(2L).email("rachel@pearson.com").build());
@@ -20,6 +23,12 @@ public class UsersController {
     users.add(User.builder().id(4L).email("jessica@pearson.com").build());
     users.add(User.builder().id(5L).email("donna@pearson.com").build());
     users.add(User.builder().id(6L).email("louis@pearson.com").build());
+
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
 
     return users;
   }
